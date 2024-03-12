67e3dd0c9697121367c807eaa6ecba7e3753e46d
antoniPrz <antoni.protoni@gmail.com>
2024-03-12 07:54:56 -0300

    feat
    rating component, product Screen, route produc screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Header from './components/Header'
 import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
-import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,45 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
-
-} from 'react-router-dom'
+  RouterProvider,
+} from "react-router-dom";
 //---- For original bootstrap styles uncomment this line below ---///
-// import 'bootstrap/dist/css/bootstrap.min.css' 
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
 //---- For original bootstrap styles comment the 2 next lines below ---///
-import './assets/styles/bootstrap.custom.css'
-import './assets/styles/index.css'
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import HomeScreen from './screens/HomeScreen';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
-
-//------------React-router-dom-------------------------------------------/// 
+//------------React-router-dom-------------------------------------------///
 const router = createBrowserRouter(
-createRoutesFromElements(
-  <Route path = "/" element = {<App/>} >
-    <Route index = { true} path='/' element={<HomeScreen/>} />
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path='/product/:id'  element= {<ProductScreen/>}/>
+    </Route>
+  )
+);
 
-  </Route>
-)
+//------------React-router-dom end-------------------------------------------///
 
-)
-
-
-//------------React-router-dom end-------------------------------------------/// 
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> // Before React-router-dom*/ } 
-    <RouterProvider router={router}/>
+    {/* <App /> // Before React-router-dom*/}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 

5a515b852229372d59584d5b1f6e02e1a2a59c50
antoniPrz <antoni.protoni@gmail.com>
2024-04-10 12:36:19 -0400

    feat
    middlewere

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,17 +1,26 @@
 import express from 'express';
 const router = express.Router();
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js'
+// import products from "../data/products.js";
 
-import products from "../data/products.js";
 
 
-
-router.get("/", (req, res) => {
+router.get("/", asyncHandler(async (req, res) => {
+    const products = await Product.find({});
     res.json(products);
-  });
+  }));
   
-  router.get("/:id", (req, res) => {
-    const product = products.find((item) => item._id === req.params.id);
-    res.json(product);
-  });
+  router.get("/:id", asyncHandler(async (req, res) => {
+    // const product = products.find((item) => item._id === req.params.id);
+    const product = await Product.findById(req.params.id);
+
+    if (product){
+       return  res.json(product);
+    }
+
+    res.status(404).json({message:"Producto no encontrado"})
+
+  }));
 
 export default router;

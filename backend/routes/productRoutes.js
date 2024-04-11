c367ce12f48439584068bb7d155169d424b427cc
antoniPrz <antoni.protoni@gmail.com>
2024-04-11 09:21:08 -0400

    feat
    middleware: errorMiddleware, controller : productController

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,26 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js'
+import { getProducts, getProductsbyId } from '../controllers/productController.js';
+
 // import products from "../data/products.js";
 
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductsbyId);
 
 
-router.get("/", asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  }));
-  
-  router.get("/:id", asyncHandler(async (req, res) => {
-    // const product = products.find((item) => item._id === req.params.id);
-    const product = await Product.findById(req.params.id);
-
-    if (product){
-       return  res.json(product);
-    }
-
-    res.status(404).json({message:"Producto no encontrado"})
-
-  }));
 
 export default router;
